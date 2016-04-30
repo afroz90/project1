a8aafc17a4f4708280d6055704be6d839035817c
Alam <Alam@Afroz>
2016-04-30 16:05:36 +0530

    2 check in

--- GitProject/src/com/git/pack/Git.java
+++ GitProject/src/com/git/pack/Git.java
@@ -4,6 +4,7 @@ public class Git {
 	public static void main(String args[]) {
 		System.out.println("this is gib based pro");
 		System.out.println("1 check in ");
+		System.out.println("2 check in");
 	}
 
 }
